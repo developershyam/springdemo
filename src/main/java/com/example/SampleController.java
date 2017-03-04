1a4fa02c1a9750f6924928d6cb82b2f9f388d7c5
shyam <developer.shyampareek@gmail.com>
2017-03-04 18:34:47 +0530

    test

--- src/main/java/com/example/SampleController.java
+++ src/main/java/com/example/SampleController.java
@@ -1,16 +1,16 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
-	
-	@GetMapping("/test")
-	public String test(){
-		
-		
+	@RequestMapping(value = { "/", "/test" })
+	@ResponseBody
+	public String test() {
+
 		return "Spring Boot running......";
 	}
 }
